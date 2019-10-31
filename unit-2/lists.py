749a6bfba235efa541a2072b88edd2713cf833d1
lizhi zhang <=>
2019-10-30 20:18:33 -0400

    lists and loops

---
+++ unit-2/lists.py
@@ -0,0 +1,47 @@
+'''
+cohort_4 = ['Daniels', 'Kyle', 'Emma', 'Julian', 'Chizea', 'Sahil', 'Shilaj', 'Lizhi', 'Keethri', 'Christina', 'Allaina']
+
+print(len(cohort_4))
+
+#access items in list using position (index)
+
+print(cohort_4[0]) #prints first item in list
+
+print(cohort_4)
+
+#add items to the list, use append
+cohort_4 .append('princeton')
+
+print(cohort_4)
+#print(type(cohort_4))
+
+
+#remove an item for the list, use remove
+
+cohort_4 . remove('Julian')
+print(cohort_4)
+
+
+cohort_4.remove('Julian')
+'''
+'''
+#create a new list with only the floats
+values = [2.3, 45, 11, -5, 3.5, 7.9, 11.7, 40, 85.6, 77.1]
+
+if number in values:
+    number = (type is integer):
+    values . remove('integer')
+print(values)
+
+'''
+#create a new list with only the floats
+values = [2.3, 45, 11, -5, 3.5, 7.9, 11.7, 40, 85.6, 77.1]
+
+float_list = []
+for value in values:
+    if type(value) is float:
+        float_list.append(value)
+
+print(float_list)
+
+
--- unit-2/loops.py
+++ unit-2/loops.py
@@ -20,7 +20,7 @@ for count in range(1, 11):
 for num in range (1, 11):
     if num % 2 == 1:
         print(num)
-'''
+
 
 #print the sum of the even numbers
 total=0
@@ -49,4 +49,77 @@ for letter in name:
 
 
 
+my_number = [3, 5, 17, 11, 21, 53, -10, -27, 45, 80]
 
+smallest_num = 0
+
+for smallest_num in my_number:
+    if number < my_number:
+        samllest_num = number
+print(smallest_num)
+
+
+
+my_number = [3, 5, 17, 11, 21, 53, -10, -27, 45, 80]
+
+smallest = 0
+
+for number in my_number:
+    if number < smallest:
+        smallest = number
+
+print(smallest)
+
+
+#find the average of each list, store in a new list
+weights = [[50, 25, 75], [95.7, 38.3, 55.2,],[ 88, 45, 16]]
+
+averages = []
+
+for item in weights:
+
+    total = 0
+    for value in item:
+        total += value
+
+    averages.append(total/len(item))
+
+print(averages)        
+
+
+*
+**
+***
+****
+*****
+******
+*******
+********
+*********
+**********
+'''
+
+for row in range(1, 11):
+    for col in range(1, row + 1):
+        print('*',  end = ' ')
+    print() 
+
+#using indexed to access list items
+#use index if we need to edit items in list
+
+#set all the negative readings to 0
+readings = [3.5, -7.7, -9.8, 13.6, 22.5, 19.7, -8.6]
+
+for idx in range(len(readings)):
+    if readings[idx] < 0:
+        readings[idx] = 0
+print(readings)
+
+
+#find the position of an item in a list
+current_age = 25
+ages = [15, 17, 27, 35, 12, 25, 55, 40, 31, 20]
+
+for idx in range(len(ages)):
+    if ages [idx] == current_age:
+        print('25 in found at position', idx)
